41e8a6da7fc62c03d3994e79cc0e2db21f8475ab
elisammg <elisammg@outlook.com2git config --global user.email elisammg@outlook.com>
2019-09-23 17:43:57 -0600

    Atom

--- DW/webpack.config.js
+++ DW/webpack.config.js
@@ -1,7 +1,25 @@
-module.exports ={
-	entry: './app.js',
-	output : {
-		path: _dirname
-		filename: 'bundle.js'
-	}
+// Basic configuration to use Javascript with
+// Ecmascript6 and CSS 
+module.exports = {
+  entry: './src/js/app.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders : [
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude:/node_modules/,
+        query: {
+          presets:['es2015']
+        }
+      }
+    ]
+  }
 }
