4962168692166e8abe6f38b6d82d672b1d90e0f7
Kevin Champney <kevinchampney@hotmail.com>
2019-10-07 10:57:36 -0600

    Add admin area

--- revista/config.php
+++ revista/config.php
@@ -1,12 +1,12 @@
 <?php 
 	session_start();
     // connect to database
-	$conn = mysqli_connect("localhost", "root", "AlexanderLeo22", "myBlog");
+	$conn = mysqli_connect("localhost", "root", "", "myblog");
 
 	if (!$conn) {
 		die("Error connecting to database: " . mysqli_connect_error());
 	}
     // define global constants
 	define ('ROOT_PATH', realpath(dirname(__FILE__)));
-	define('BASE_URL', 'http://localhost/myBlog/');
+	define('BASE_URL', 'http://localhost/RevistaWeb/revista/');
 ?>
