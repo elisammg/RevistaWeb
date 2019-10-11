e52cc374d17c9c5731245c3b2d6b58597c28453d
Kevin Champney <kevinchampney@hotmail.com>
2019-10-10 18:51:23 -0600

    Arreglos en folder admin

--- revista/admin/includes/menu.php
+++ revista/admin/includes/menu.php
@@ -4,10 +4,10 @@
 			<h2>Actions</h2>
 		</div>
 		<div class="card-content">
-			<a href="<?php echo BASE_URL . 'admin/create_post.php' ?>">Create Posts</a>
-			<a href="<?php echo BASE_URL . 'admin/posts.php' ?>">Manage Posts</a>
-			<a href="<?php echo BASE_URL . 'admin/users.php' ?>">Manage Users</a>
-			<a href="<?php echo BASE_URL . 'admin/topics.php' ?>">Manage Topics</a>
+			<a href="<?php echo 'create_post.php' ?>">Create Posts</a>
+			<a href="<?php echo 'posts.php' ?>">Manage Posts</a>
+			<a href="<?php echo 'users.php' ?>">Manage Users</a>
+			<a href="<?php echo 'topics.php' ?>">Manage Topics</a>
 		</div>
 	</div>
 </div>
