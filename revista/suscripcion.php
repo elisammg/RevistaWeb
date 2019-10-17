aebef919c1b91e96e519a63b640691b95eb3dd11
Kevin Champney <kevinchampney@hotmail.com>
2019-10-17 17:24:30 -0600

    Merge con Elisa

--- revista/suscripcion.php
+++ revista/suscripcion.php
@@ -15,6 +15,28 @@
   <?php require_once('includes/header.php') ?>
 </header>
   <body>
+   <?php 
+    if (isset($_GET['id_users']))
+     {
+      $id = ($_GET['id_users']);
+      $infosusc = mysqli_query("SELECT * FROM cobro WHERE id_users = '$id'");
+      $use = mysqli_fetch_array($infosusc);
+    } 
+    ?>
+
+    <?php 
+      if (isset($_POST['enviar']))
+      {
+        $id = $_SESSION['users']['id'];
+        $suscripcion=$_POST['pokemon'];
+        $numero=$_POST['notarjeta'];
+        $date=$_POST['fecha'];
+        $seguridad=$_POST['tres'];
+        $sql = "INSERT INTO cobro (id_users, id_sus, tarjeta, vencimiento, atras, iniciosusc) VALUES ('$id', '$suscripcion', '$numero', '$date', '$seguridad')";
+        $result = mysqli_query($conexion, $sql);
+        echo ("Se ingresaron correctamente los datos");
+      }
+    ?>
   <?php require_once('includes/susc.php') ?>
 
 	<script src="js/vendor/jquery.js"></script>
