df0929cc94b65727ff36c97ecf842dd1fd489df6
elisammg <elisammg@outlook.com2git config --global user.email elisammg@outlook.com>
2019-10-26 09:25:06 -0600

    diseño

--- revista/aboutus.php
+++ revista/aboutus.php
@@ -32,6 +32,14 @@
             </div>
           </div>
         <div class="large-12 cell">
+           <div class="callout"> 
+           <h2>Sobre la pagina</h2>
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+           </p> 
+           </div>   
+        </div>
+        <div class="large-12 cell">
           <div class="callout"> 
           <h3>Roles</h3>
           <div class="grid-x grid-padding-x">
@@ -59,25 +67,27 @@
           </div>        
         </div>
          <div class="large-12 cell">
-           <div class="full-width-testimonial">
-  <div class="full-width-testimonial-section">
-    <div class="full-width-testimonial-icon text-center">
-      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-         width="41px" height="34px" viewBox="-235 240 41 34" style="enable-background:new -235 240 41 34;" xml:space="preserve">
-          <path class="quote-path" d="M-231.3,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4
-            h8.1v12.8h-14.4V260.4z"/>
-          <path class="quote-path" d="M-212,260.4c0-5,1.3-8.8,3.7-11.7c2.4-2.8,6-4.6,10.5-5.5v5c-3.5,1-6,2.8-7.1,5.5c-0.7,1.4-0.9,2.8-0.8,4h8.1
-            v12.8H-212V260.4z"/>
-      </svg>
-    </div>
-    <div class="full-width-testimonial-content">
-      <h5 class="full-width-testimonial-text">RESUMEN ARTÍCULO</h5>
-    </div>
-  </div>
-</div>      
+           <div class="callout">
+           <div class="grid-x grid-padding-x">
+            <div class="large-6 medium-6 cell"> 
+           <h2>Categorías</h2>
+           </div>
+         <div class="large-6 medium-6 cell"> 
+           <h2>SubCategorias</h2>
+         </div>
+        </div>   
         </div>
       </div>
+      <div class="large-12 cell">
+           <div class="callout"> 
+           <h2>Sobre los articulos</h2>
+           <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+           </p> 
+           </div>   
+        </div>
     </div>
+  </div>
 
     <script src="js/vendor/jquery.js"></script>
     <script src="js/vendor/what-input.js"></script>
--- revista/includes/header.php
+++ revista/includes/header.php
@@ -2,7 +2,6 @@
   <h1 class="subnav-hero-headline">REVISTA WEB<small>UNIS</small></h1>
   <ul class="subnav-hero-subnav">
   	<li><a href="index.php">Inicio</a></li>
-    <li><a href="aboutus.php">Sobre nosotros</a></li>
     <li>
       <a data-open="exampleModal1" >Ingresar</a>
       <div class="reveal" id="exampleModal1" data-reveal>
