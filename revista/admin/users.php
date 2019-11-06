cb184af5a938da88af4e05dd717a9a37b3ff4e9e
Kevin Champney <kevinchampney@hotmail.com>
2019-11-06 10:40:26 -0600

    Paginacion en admin.php

--- revista/admin/users.php
+++ revista/admin/users.php
@@ -1,7 +1,43 @@
 <?php 
+	
+	
+	//Cantidad de resultados por página (debe ser INT, no string/varchar)
+	$limit = 5;
+
+	//Comprueba si está seteado el GET de HTTP
+	if (isset($_GET["pagina"])) {
+
+		//Si el GET de HTTP SÍ es una string / cadena, procede
+		if (is_string($_GET["pagina"])) {
+
+			//Si la string es numérica, define la variable 'pagina'
+			if (is_numeric($_GET["pagina"])) {
+
+				//Si la petición desde la paginación es la página uno
+				//en lugar de ir a 'index.php?pagina=1' se iría directamente a 'index.php'
+				if ($_GET["pagina"] == 1) {
+					header("Location: admin.php");
+					die();
+				} else { //Si la petición desde la paginación no es para ir a la pagina 1, va a la que sea
+					$pagina = $_GET["pagina"];
+				};
+
+			} else { //Si la string no es numérica, redirige al index (por ejemplo: index.php?pagina=AAA)
+				header("Location: admin.php");
+				die();
+			};
+		};
+
+	} else { //Si el GET de HTTP no está seteado, lleva a la primera página (puede ser cambiado al index.php o lo que sea)
+		$pagina = 1;
+	}
+
+	//Define el número 0 para empezar a paginar multiplicado por la cantidad de resultados por página
+	$offset = ($pagina-1) * $limit;
+	
 	// Get all admin users from DB
-	$admins = getAdminUsers();
-	$roles = ['Admin', 'Author', 'Lector', 'Moderador'];				
+	$admins = getAdminUsers($limit, $offset);
+	$roles = ['Admin', 'Author', 'Lector', 'Moderador'];
 ?>
 
 <body>
@@ -67,8 +103,17 @@
 					</tbody>
 				</table>
 			<?php endif ?>
+			<?php
+				//Crea un bucle donde $i es igual 1, y hasta que $i sea menor o igual a X, a sumar (1, 2, 3, etc.)
+				//Nota: X = $total_paginas
+				for ($i=1; $i<=$total_paginas; $i++) {
+					//En el bucle, muestra la paginación
+					echo "<a href='?pagina=".$i."'>".$i."</a> | ";
+				}
+			?>
 		</div>
 		<!-- // Display records from DB -->
+		
 	</div>
 </body>
 </html>
