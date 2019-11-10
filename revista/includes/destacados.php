5cd7d159ec91c77f17dc805e53a5a1eb9a5ad874
elisammg <elisammg@outlook.com2git config --global user.email elisammg@outlook.com>
2019-11-09 21:00:27 -0600

    solo si tiene susc aparecen los datos de cobro

--- revista/includes/destacados.php
+++ revista/includes/destacados.php
@@ -5,14 +5,14 @@ if(isset($_GET['destacado'])){
   if(!empty($_GET['destacar'])) {
 
 
-   /* $checked_count = count($_GET['destacar']);
+   $checked_count = count($_GET['destacar']);
       echo "You have selected following ".$checked_count." option(s): <br/>";*/
 
 
     foreach($_GET['destacar'] as $selected) {
       $sqldestacado = "UPDATE `posts` SET `destacado` = 1 WHERE `posts`.`id` = '$selected'";
        $resultado0 = mysqli_query($conexion, $sqldestacado);
-
+       echo "<p>" . $selected . "</p>";
         }
       
         }else{
